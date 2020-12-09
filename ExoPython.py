1ae0ad41854c0f83d41958981573941ed1ab112a
Ash2oPS <ash2o@outlok.fr>
2020-12-09 15:04:31 +0100

    Update ExoPython.py

--- ExoPython.py
+++ ExoPython.py
@@ -3,16 +3,22 @@ init()
 from colorama import Fore, Back, Style
 import random
 
+finDuJeu = False
 motsPossibles = ["suivre", "survie", "motard", "joyaux", "jockey", "aboyer", "angles", "animee", "canifs", "burger"]
-indexMot = random.randint(0, 9)
+indexMot = random.randint(0, len(motsPossibles) + 1)
 motADeviner = motsPossibles[indexMot]
 motPropose = ""
 
-while (len(motPropose) != 6):
-    motPropose = input("Quel mot proposez-vous de 6 caractères proposez-vous ?\n")
-    if (len(motPropose) != 6):
-        print("Votre mot n'est pas composé de 6 caractères.")
 
+while (finDuJeu == False):                                                                      #   DEBUT DE LA BOUCLE DE JEU
+
+
+    while (len(motPropose) != 6):                                                               #
+        motPropose = input("Quel mot proposez-vous de 6 caractères proposez-vous ?\n")          #   VERIFIE QUE LE MOT
+        if (len(motPropose) != 6):                                                              #   CONTINENE BIEN 6 LETTRES
+            print("Votre mot n'est pas composé de 6 caractères.")                               #
+    
+    print("Vous avez proposé le mot \"", motPropose , "\".")
 
 
 
