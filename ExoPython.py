369df0df292b9e642cc50161e02a73a89b96b484
Ash2oPS <ash2o@outlok.fr>
2020-12-09 14:41:10 +0100

    Update ExoPython.py

--- ExoPython.py
+++ ExoPython.py
@@ -1,1 +1,12 @@
-f
+'''SUIVRE SURVIE MOTARD JOYAUX JOCKEY ABOYER ANGLES ANIMEE CANIFS BURGER'''
+
+
+from colorama import init
+init()
+from colorama import Fore, Back, Style
+print(Fore.RED + 'some red text', end=" ")
+print(Back.GREEN + 'and with a green background')
+print(Style.DIM + 'and in dim text')
+print(Style.RESET_ALL)
+print('back to normal now')
+input()
